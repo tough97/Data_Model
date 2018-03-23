4be46a324deea593864b2768fbbfbb67b1eef702
gang-liu <627420278@qq.com>
2018-03-23 17:33:21 +0800

    Bug fixed, Tree IO functionality test passes
    TODO : try to implement tree searching function

---
+++ data_model/src/main/java/com/ynjs/data/query/Filter.java
@@ -0,0 +1,12 @@
+package com.ynjs.data.query;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * Filter is used to perform tree traversal and exporting to
+ * JSON
+ */
+
+public interface Filter {
+
+}
